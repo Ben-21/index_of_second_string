d34f0a4609dcf572efd00b17fd088bc94537067a
Ben-21 <bs@schaefer-inet.de>
2023-06-08 11:22:27 +0200

    implemented test 1

--- src/main/java/model/SecondString.java
+++ src/main/java/model/SecondString.java
@@ -1,4 +1,12 @@
 package model;
 
 public class SecondString {
+
+
+    public int secondSymbol(String word, char letter){
+
+        return 4;
+    }
+
+
 }
--- src/test/java/SecondStringTest.java
+++ src/test/java/SecondStringTest.java
@@ -1,9 +1,24 @@
+import model.SecondString;
 import org.junit.jupiter.api.Test;
 
 public class SecondStringTest {
 
 
+@Test
+    void returnsIndexOfSecondLetter(){
+    String input = "hello";
+    char letter = 'l';
 
+
+    SecondString secondString = new SecondString();
+
+
+    int actual = secondString.secondSymbol(input, letter);
+    int expected = 3;
+
+
+
+}
 
 
 
